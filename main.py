5f24b62c97376035a26d323181104ffe250b13cb
Bibekkun <bibekrai655@gmail.com>
2023-04-02 16:40:58 +0545

    using seleniun instead of beautiful soup

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
+driver_path='/Users/bibekrai/workspace/Daraz web scrapper/chromedriver'
 datalist=[]
-urls=['https://www.daraz.com.np/products/apple-macbook-air-13-in-with-m1-chip256gb-with-1-year-insurance-breakageliquid-damage-evostore-i105118187-s1026752551.html?spm=a2a0e.searchlistcategory.list.1.4231383a4vH83b&search=1']
+url=['https://www.daraz.com.np/products/apple-macbook-air-13-in-with-m1-chip256gb-with-1-year-insurance-breakageliquid-damage-evostore-i105118187-s1026752551.html?spm=a2a0e.searchlistcategory.list.1.4231383a4vH83b&search=1']
+driver = webdriver.Chrome(driver_path)
+driver.get(url)
 
-for url in urls:
-    response=requests.get(url)
-    html_content=response.content
+price_element = driver.find_element_by_xpath('//div[@class="pdp-product-price"]//span[@class="pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl"]')
+price=price_element.text.strip()
+data={
+    'Price':price
+}
+datalist=[data]
+df = pd.DataFrame(datalist)
 
-    soup=BeautifulSoup(html_content,'html.parser')
-    
-    
-    Price=soup.find('div',{"class":'pdp-product-price'}).find('span',{"class":'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl'}).text.strip()
-    data={
-        'Price':Price
-    }
-    datalist.append(data)
-df=pd.DataFrame(datalist)
-df.to_csv('daraz.csv',index=True)
+df.to_csv('daraz.csv', index=True)
+
+driver.quit()
